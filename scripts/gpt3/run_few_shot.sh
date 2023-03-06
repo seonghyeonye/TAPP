0967fef283b085b0d95441d9cd72d26b23dadae5
hbin0701 <hbin0701@kaist.ac.kr>
2023-03-06 02:31:43 +0000

    fix gpt few shot

--- scripts/gpt3/run_few_shot.sh
+++ scripts/gpt3/run_few_shot.sh
@@ -1,23 +1,31 @@
-CUDA_VISIBLE_DEVICES=8,9,10,11,12,13,14,15 python src/run_autoreg.py \
-    --do_predict \
-    --predict_with_generate \
-    --evaluation_strategy "no" \
-    --model_name_or_path facebook/opt-30b \
-    --max_source_length 1024 \
-    --max_target_length 64 \
-    --generation_max_length 64 \
-    --max_num_instances_per_task 100 \
-    --max_num_instances_per_eval_task 100 \
-    --add_task_name False \
+export openai_key=""
+
+data_dir=data/splits/default
+task_dir=data/tasks
+output_dir=output/few_shot_davinci 
+max_num_instances_per_eval_task=100
+
+type="few-shot" 
+echo ${type}
+
+for engine in "davinci"
+do
+echo $engine
+python src/run_gpt3.py \
+    --data_dir $data_dir \
+    --task_dir $task_dir \
+    --overwrite_cache \
+    --max_num_instances_per_task 1 \
+    --max_num_instances_per_eval_task ${max_num_instances_per_eval_task} \
     --add_task_definition True \
     --num_pos_examples 8 \
     --num_neg_examples 0 \
     --add_explanation False \
-    --tk_instruct False \
-    --data_dir data/splits/default \
-    --task_dir data/tasks \
-    --output_dir output/default/opt-30b-revised/Zero-shot \
-    --overwrite_output_dir \
-    --cache_dir ./cache/ \
-    --overwrite_cache \
-    --per_device_eval_batch_size 1
+    --max_source_length 2048 \
+    --max_target_length 128 \
+    --engine ${engine} \
+    --output_dir ${output_dir} \
+    --icil False
+
+python src/compute_metrics.py --predictions ${output_dir}/predicted_examples.jsonl --track default --compute_per_category_metrics --compute_per_task_metrics
+done
